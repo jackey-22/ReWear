b19c663d413aefdff016c4b96f449d5b664031af
jackey-22 <jackeychandani123@gmail.com>
2025-07-12 10:49:31 +0530

    [FEAT] client  route set and  env.example set

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ReWear
+# ReWear – Community Clothing Exchange
+
+Chandani Jaykishan Mahendrabhai -jackeychandani123@gmail.com -TL
+Patel Khushi Nirajkumar -khushipatela15@gmail.com
+Hihoriya Nitigna Mukeshbhai -nitignahihoriya@gmail.com
+Gohil Jay Jagdishbhai -jgohil22042006@gmail.com
--- client-web/src/routes.jsx
+++ client-web/src/routes.jsx
@@ -2,28 +2,10 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 // import { loginLoader, verifyLoader } from './loaders/verify.loader';
 import ErrorElement from './components/ErrorElement';
-// import Login from './pages/Login';
-// import AddFoodItem from './pages/service-provider/AddFoodItem';
-// import AdminDashboard from './pages/admin/Dashboard';
-// import AddAccommodation from './pages/service-provider/AddAccommodation';
-// import CustomerRegistration from './pages/CutomerRegistration';
-// import ServiceProviderRegistration from './pages/ServiceProviderRegistration';
-// import VerifySP from './pages/admin/VerifySP';
-// import FoodSection from './pages/customer/FoodSection';
-// import FoodCart from './pages/customer/FoodCart';
-// import ServiceProviderList from './pages/customer/ServiceProviderList';
-// import ServiceProviderDetail from './pages/customer/ServiceProviderDetail';
-// import AccommodationProviderList from './pages/customer/AccommodationProviderList';
-// import AccommodationProviderDetail from './pages/customer/AccommodationProviderDetail';
-// import FoodProvidersPage from './pages/admin/ViewFoodOrder';
-// import FoodProviderOrdersPage from './pages/admin/FoodOders';
-// import FoodCheckout from './pages/customer/FoodCheckout';
-// import FoodOrderHistory from './pages/customer/FoodOrderHistory';
-// import ProfilePage from './pages/service-provider/Profile';
-// import ManageCustomers from './pages/admin/ManageCustomer';
-// import FoodServiceDashboard from './pages/service-provider/FoodServiceDashboard';
-// import AccommodationServiceDashboard from './pages/service-provider/AccommodationServiceDashboard';
-// import ManageSP from './pages/admin/ManageSp';
+import Login from './pages/Login';
+import AdminDashboard from './pages/admin/Dashboard';
+import VerifyProduct from './pages/admin/VerifyProduct';	
+
 
 const routes = createBrowserRouter([
 	{
@@ -31,60 +13,41 @@ const routes = createBrowserRouter([
 		element: <HomePage />,
 		// loader: loginLoader,
 	},
-	// {
-	// 	path: '/customer-registration',
-	// 	element: <CustomerRegistration />,
-	// },
+	{
+		path: '/',
+		element: <ShowProduct/>,
+		// loader: loginLoader,
+	},
+	{
+		path: '/login',
+		element: <Login.jsx/>,
+		// loader: loginLoader,
+	},
+	{
+		path: '/Register',
+		element: <Register />,
+		// loader: loginLoader,
+	},
 
-	// {
-	// 	path: '/service-provider-registration',
-	// 	element: <ServiceProviderRegistration />,
-	// },
-	// {
-	// 	path: '/admin',
-	// 	// element: <AdminLayout />, // optional layout if needed
-	// 	// loader: verifyLoader,
-	// 	// errorElement: <ErrorElement />,
-	// 	children: [
-	// 		{ path: 'dashboard', element: <AdminDashboard /> },
-	// 		{ path: 'verify', element: <VerifySP /> },
-	// 		{ path: 'food-providers', element: <FoodProvidersPage /> },
-	// 		{ path: 'manage-customers', element: <ManageCustomers /> },
-	// 		{ path: '/admin/food-providers/:id', element: <FoodProviderOrdersPage /> },
-	// 		{ path: 'manage-sp', element: <ManageSP /> },
-	// 	],
-	// },
-	// {
-	// 	path: '/service-provider',
-	// 	// element: , // Dashboard avse ahiya
-	// 	// loader: verifyLoader,
-	// 	// errorElement: <ErrorElement />,
-	// 	children: [
-	// 		{ path: 'add-food', element: <AddFoodItem /> },
-	// 		{ path: 'add-accommodation', element: <AddAccommodation /> },
-	// 		{ path: 'profile/:id', element: <ProfilePage /> },
-	// 		{ path: 'foodservice-dashboard', element: <FoodServiceDashboard /> },
-	// 		{ path: 'accommodationservice-dashboard', element: <AccommodationServiceDashboard /> },
-	// 	],
-	// },
-	// {
-	// 	path: '/customer',
-	// 	// element: , // Dashboard avse ahiya
-	// 	// loader: verifyLoader,
-	// 	// errorElement: <ErrorElement />,
-	// 	children: [
-	// 		{ path: 'food', element: <ServiceProviderList /> },
-	// 		{ path: '/customer/food-service-provider/:id', element: <ServiceProviderDetail /> },
-	// 		{ path: 'cart', element: <FoodCart /> },
-	// 		{ path: 'accommodations', element: <AccommodationProviderList /> },
-	// 		{
-	// 			path: '/customer/accommodation-service-provider/:id',
-	// 			element: <AccommodationProviderDetail />,
-	// 		},
-	// 		{ path: 'food-checkout', element: <FoodCheckout /> },
-	// 		{ path: 'order-history', element: <FoodOrderHistory /> },
-	// 	],
-	// },
+	{
+		path: '/admin',
+		// element: <AdminLayout />, // optional layout if needed
+		// loader: verifyLoader,
+		errorElement: <ErrorElement />,
+		children: [
+			{ path: 'dashboard', element: <AdminDashboard /> },
+			{ path: 'verifyProduct', element: <VerifyProduct /> },
+		],
+	},
+	{
+		path: '/user',
+		element: <Dashboard />	, // Dashboard avse ahiya
+		//loader: verifyLoader,
+		errorElement: <ErrorElement />,
+		children: [
+			{ path: '/AddProduct', element: <ServiceProviderList /> },	
+		],
+	},
 	{
 		path: '*',
 		element: <ErrorElement />,
